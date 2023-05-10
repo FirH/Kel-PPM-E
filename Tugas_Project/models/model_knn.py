d0fbf47729c289c8426301fb7223b46677ec8b46
Firhan Imam Haekal <firhanimamhaekal@gmail.com>
2023-05-10 18:38:11 +0700

    Push

---
+++ Tugas_Project/models/model_knn.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def jarakEu(data1, data2) :
+  jarak = np.square(data1-data2)
+  jarak = np.sum(jarak)
+  return np.sqrt(jarak)
+
+from collections import Counter
+def knn(k,datalatih,labellatih,datauji) :
+  jarak = np.array([jarakEu(datalatih.iloc[x], datauji) for x in range (datalatih.shape[0])])
+  indeks_k_minimum = jarak.argsort()[:k]
+  k_kelas = labellatih.iloc[indeks_k_minimum].to_numpy()
+  counter = Counter(k_kelas)
+  kelas_uji = counter.most_common(1)[0][0]
+  return kelas_uji
