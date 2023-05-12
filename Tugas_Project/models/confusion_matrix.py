200821ec29cca7ad1459fdd2aef1693f56e4f972
FathanTH <fathan.thoriq.h@student.ub.ac.id>
2023-05-12 10:59:15 +0700

    removed data splitting

--- Tugas_Project/models/confusion_matrix.py
+++ Tugas_Project/models/confusion_matrix.py
@@ -1,24 +1,20 @@
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 
-def report_nb(df,model) :
-    data_latih, data_uji = train_test_split(df, test_size=0.2, random_state=42)
+def report_nb(data_latih, data_uji,model) :
     predictions = model.predict(data_latih, data_uji)
     print(classification_report(data_uji[data_uji.columns[-1]], predictions))
 
-def report_knn(df,model) :
-    data_latih, data_uji = train_test_split(df, test_size=0.2, random_state=42)
+def report_knn(data_latih, data_uji,model) :
     predictions = model.predict(data_latih, data_uji)
     print(classification_report(data_uji[data_uji.columns[-1]], predictions))
 
-def report_svm(df,model) :
-    data_latih, data_uji = train_test_split(df, test_size=0.2, random_state=42)
+def report_svm(data_latih, data_uji,model) :
     model.fit(data_latih, data_latih[data_latih.columns[-1]])
     predictions = model.predict(data_uji)
     print(classification_report(data_uji[data_uji.columns[-1]], predictions))
 
-def report_dt(df,model):
-    data_latih, data_uji = train_test_split(df, test_size=0.2, random_state=42)
+def report_dt(data_latih, data_uji,model):
     model.fit(data_latih, data_latih.columns[-1])
     data_uji_dict = data_uji.iloc[:,:-1].to_dict(orient = "records")
     predictions = []
